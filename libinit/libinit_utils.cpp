af3ea5c57252f01ed214c46bc99dfb7a205f8608
Waxaranai <wisnu@without.live>
2021-10-11 23:11:11 +0700

    sm6150-common: fp: build `Redfin 12 SPB5.210812.002`
    Signed-off-by: Waxaranai <wisnu@without.live>

--- libinit/libinit_utils.cpp
+++ libinit/libinit_utils.cpp
@@ -10,6 +10,8 @@
 
 #include <libinit_utils.h>
 
+using std::string;
+
 void property_override(std::string prop, std::string value, bool add) {
     auto pi = (prop_info *) __system_property_find(prop.c_str());
     if (pi != nullptr) {
@@ -30,13 +32,21 @@ std::vector<std::string> ro_props_default_source_order = {
 
 void set_ro_build_prop(const std::string &prop, const std::string &value, bool product) {
     std::string prop_name;
+    string safetynet = "green";
+
+    string fp = "google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys";
+    string desc = "redfin-user 12 SPB5.210812.002 7671067 release-keys";
 
     for (const auto &source : ro_props_default_source_order) {
         if (product)
             prop_name = "ro.product." + source + prop;
         else
             prop_name = "ro." + source + "build." + prop;
-
         property_override(prop_name, value, true);
+	property_override(string("ro.") + prop + string("build.fingerprint"), fp);
+	property_override(string("ro.") + prop + string("build.description"), desc);
     }
+    // Set safetynet workaround
+    property_override("ro.boot.verifiedbootstate", safetynet);
+    property_override(string("ro.build.fingerprint"), fp);
 }
