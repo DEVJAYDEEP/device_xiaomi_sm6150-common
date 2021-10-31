72d10038add01d63cea025c56cb4aad47b49f874
venom-stark <sachidanandabehera562@gmail.com>
2021-10-31 13:35:34 +0530

    sm6150-common: Build more powerhal

--- sm6150.mk
+++ sm6150.mk
@@ -332,6 +332,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2 \
+	android.hardware.power@1.2.vendor \
     android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service.xiaomi-libperfmgr
 
